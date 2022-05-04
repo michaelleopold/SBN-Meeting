0a83f608a649e2e7f10988fc24ecf1cb22ab17b8
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-04 23:51:44 +0700

    Third commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center">
-      <h1 className="text-6xl">SBN MEETING</h1>
+      <h1 className="text-7xl tracking-wide">SBN MEETING</h1>
+      <h1 className="text-3xl text-gray-500 mt-4 font-mono">(comming soon)</h1>
     </div>
   )
 }
