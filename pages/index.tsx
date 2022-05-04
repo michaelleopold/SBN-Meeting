8beab421c9b73fcaeba4020350d8861beeb5e7a1
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-04 23:40:56 +0700

    Second commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="text-6xl">SBN MEETING</h1>
+    </div>
+  )
+}
+
+export default Home
