0694a46dabfc6c526ffee74156d4f079c793ba85
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-09 01:31:10 +0700

    test without next-transpile-module

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-const withTM = require('next-transpile-modules')(['@jitsi/react-sdk']);
+module.exports = nextConfig
 
-module.exports = withTM({
-  reactStrictMode: true
-})
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// const withTM = require('next-transpile-modules')(['@jitsi/react-sdk']);
+
+// module.exports = withTM({
+//   reactStrictMode: true
+// })
