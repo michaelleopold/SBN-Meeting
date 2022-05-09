2350a3b7d79a45f1ecc8c398fafb2f66ce801e3d
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-09 18:01:09 +0700

    add webpack handler

--- next.config.js
+++ next.config.js
@@ -3,7 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+      if (!isServer) {
+          // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+          config.resolve.fallback = {
+              fs: false
+          }
+      }
+
+      config.reactStrictMode = true
+
+      return config;
+  }
+}
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
