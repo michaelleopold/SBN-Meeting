6dccd02af574e7dfc5721cb6a8b1aa1da0271cc9
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-09 18:21:46 +0700

    add next transpile modules

--- next.config.js
+++ next.config.js
@@ -1,32 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-// module.exports = nextConfig
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-      if (!isServer) {
-          // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-          config.resolve.fallback = {
-              fs: false
-          }
-      }
-
-      config.reactStrictMode = true
-
-      return config;
-  }
-}
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: true,
 // }
 
-// const withTM = require('next-transpile-modules')(['@jitsi/react-sdk']);
+// module.exports = nextConfig
 
-// module.exports = withTM({
-//   reactStrictMode: true
-// })
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+const withTM = require('next-transpile-modules')(['@jitsi/react-sdk']);
+
+module.exports = withTM({
+  reactStrictMode: true
+})
