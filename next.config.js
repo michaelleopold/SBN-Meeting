a37de3dda6fff11423465b56fd2a2c61651549f1
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-09 20:57:42 +0700

    delete unused comment

--- next.config.js
+++ next.config.js
@@ -5,17 +5,6 @@
 
 // module.exports = nextConfig
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// const withTM = require('next-transpile-modules')(['@jitsi/react-sdk']);
-
-// module.exports = withTM({
-//   reactStrictMode: true
-// })
-
 const withTM = require('next-transpile-modules')(['@jitsi/react-sdk']);
 
 module.exports = withTM({
