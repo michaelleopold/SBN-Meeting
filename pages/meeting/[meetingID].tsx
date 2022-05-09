5d6a84f81068c365b556dc729280dddcc39f1c10
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-09 15:06:04 +0700

    add no explicit on tsconfig

--- pages/meeting/[meetingID].tsx
+++ pages/meeting/[meetingID].tsx
@@ -1,85 +1,85 @@
-// import React, { useEffect, useRef, useState } from 'react'
-// import { JitsiMeeting } from '@jitsi/react-sdk'
-// import { useRouter } from 'next/router'
+import React, { useEffect, useRef, useState } from 'react'
+import { JitsiMeeting } from '@jitsi/react-sdk'
+import { useRouter } from 'next/router'
 
-// // components
-// import Loading from '../../components/loading'
-
-// interface Props {
-
-// }
-
-// const asyncTimeout = (ms: number) => {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, ms);
-//   });
-// };
-
-// const Meeting: React.FC<Props> = () => {
-//     const router = useRouter()
-//     const query: any = router.query
-
-//     const [jitsi, setJitsi]: any = useState(null)
-
-//     const handleCloseMeeting = () => {
-//       router.replace('/')
-//     }
-
-//     useEffect(() => {
-//       if (jitsi) {
-//         jitsi.addEventListener('readyToClose', handleCloseMeeting);
-//         jitsi.addEventListener('videoConferenceLeft', handleCloseMeeting)
-//       }
-  
-//       return () => {
-//         jitsi?.removeEventListener('readyToClose', handleCloseMeeting);
-//         jitsi?.removeEventListener('videoConferenceLeft', handleCloseMeeting)
-//       };
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, [jitsi]);
-
-//     return (
-//       <JitsiMeeting
-//         domain = "meetup.hopto.org"
-//         roomName = {query.meetingID}
-//         getIFrameRef = { iframeRef => { iframeRef.style.height = '100vh'; } }
-//         onApiReady = {(haha) => {
-//           setJitsi(haha)
-//         }}
-
-//         interfaceConfigOverwrite={{
-//           DISPLAY_WELCOME_FOOTER: false,
-//           HIDE_DEEP_LINKING_LOGO: true,
-//           JITSI_WATERMARK_LINK: '#',
-//           MOBILE_APP_PROMO: false,
-//           NATIVE_APP_NAME: 'SBN MEETING',
-//           PROVIDER_NAME: 'PT. SBN',
-//           SHOW_JITSI_WATERMARK: false
-//         }}
-//         configOverwrite={{
-//           disablePolls: true,
-//           disableSelfView: true,
-
-//         }}
-//         spinner= {() => <Loading />}
-//       />
-//     );
-// }
-
-// export default Meeting
-
-import React from 'react'
+// components
+import Loading from '../../components/loading'
 
 interface Props {
 
 }
 
+const asyncTimeout = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
 const Meeting: React.FC<Props> = () => {
-  return (
-    <div className="">
-      <h1>Meeting</h1>
-    </div>
-  );
+    const router = useRouter()
+    const query: any = router.query
+
+    const [jitsi, setJitsi]: any = useState(null)
+
+    const handleCloseMeeting = () => {
+      router.replace('/')
+    }
+
+    useEffect(() => {
+      if (jitsi) {
+        jitsi.addEventListener('readyToClose', handleCloseMeeting);
+        jitsi.addEventListener('videoConferenceLeft', handleCloseMeeting)
+      }
+  
+      return () => {
+        jitsi?.removeEventListener('readyToClose', handleCloseMeeting);
+        jitsi?.removeEventListener('videoConferenceLeft', handleCloseMeeting)
+      };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [jitsi]);
+
+    return (
+      <JitsiMeeting
+        domain = "meetup.hopto.org"
+        roomName = {query.meetingID}
+        getIFrameRef = { iframeRef => { iframeRef.style.height = '100vh'; } }
+        onApiReady = {(haha) => {
+          setJitsi(haha)
+        }}
+
+        interfaceConfigOverwrite={{
+          DISPLAY_WELCOME_FOOTER: false,
+          HIDE_DEEP_LINKING_LOGO: true,
+          JITSI_WATERMARK_LINK: '#',
+          MOBILE_APP_PROMO: false,
+          NATIVE_APP_NAME: 'SBN MEETING',
+          PROVIDER_NAME: 'PT. SBN',
+          SHOW_JITSI_WATERMARK: false
+        }}
+        configOverwrite={{
+          disablePolls: true,
+          disableSelfView: true,
+
+        }}
+        spinner= {() => <Loading />}
+      />
+    );
 }
 
 export default Meeting
+
+// import React from 'react'
+
+// interface Props {
+
+// }
+
+// const Meeting: React.FC<Props> = () => {
+//   return (
+//     <div className="">
+//       <h1>Meeting</h1>
+//     </div>
+//   );
+// }
+
+// export default Meeting
