a07af6bf6e6b53b1b154be64486b76aff16ffc23
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-05-09 13:45:08 +0700

    using basic html input

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { TextField } from '@mui/material'
-import { TextField } from '@material-ui/core'
+// import { TextField } from '@material-ui/core'
 
 interface Props {
   label: string
@@ -10,17 +10,25 @@ interface Props {
 
 const Input: React.FC<Props> = ({ label, value, onChange }) => {
     return (
-        <TextField
+        // <TextField
+        //     value={value}
+        //     onChange={onChange}
+        //     autoComplete="off"
+        //     id="outlined-basic"
+        //     label={label}
+        //     variant="outlined"
+        //     className="w-full"
+        //     // InputLabelProps={{ style: { color: '#bdbdbd' } }}
+        //     // inputProps={{ style: { color: '#757575' } }}
+        //     // placeholder="ex: sundaymeeting123"
+        // />
+        <input
             value={value}
             onChange={onChange}
+            type="text"
             autoComplete="off"
-            id="outlined-basic"
-            label={label}
-            variant="outlined"
-            className="w-full"
-            // InputLabelProps={{ style: { color: '#bdbdbd' } }}
-            // inputProps={{ style: { color: '#757575' } }}
-            // placeholder="ex: sundaymeeting123"
+            placeholder="meeting id"
+            className='h-[3rem] px-2 w-full text-gray-600 border-[1px] border-gray-500 shadow-lg rounded-md active:border-blue-500 hover:border-gray-400'
         />
     );
 }
